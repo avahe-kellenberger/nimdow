6118f58d1023192672a9777a4be24d86a6b22841
Avahe Kellenberger <avahe@protonmail.ch>
2020-04-20 04:27:38 -0400

    More progress on mapping a user config to procs.
    Next/last step is to map a proc's name to an actual proc.

--- src/nimdowpkg/config/config.nim
+++ src/nimdowpkg/config/config.nim
@@ -1,14 +1,47 @@
-import parsetoml
+import 
+  "../keys/keyutils",
+  parsetoml,
+  tables,
+  x11 / xlib
 
-let defaultConfig = parsetoml.parseFile("config.default.toml")
+type KeyCombo* =
+  tuple[keycode: int, modifiers: int] 
 
-proc printActionMapping(action: string) =
-  let key = defaultConfig[action]["key"]
-  echo("Key: \n  ", key)
-  let modifiers = defaultConfig[action]["modifiers"].arrayVal
-  echo("Modifiers: ")
-  for modifier in modifiers:
-    echo "  ", modifier
+# TODO: value of this table should be "proc".
+# Need to find a way to map a string repr of a proc to the proc itself.
+var ConfigTable* = tables.initTable[KeyCombo, string]()
 
-printActionMapping("testAction")
+func xorModifiers(modifiers: openarray[TomlValueRef]): int =
+  # TODO: OR all the values together (and ensure they are ints)
+  1
+
+proc getKeyCombo(loadedConfig: TomlTable, display: PDisplay, action: string): KeyCombo =
+  var keyConfig = loadedConfig[action]["key"]
+  var modifiersConfig = loadedConfig[action]["modifiers"]
+  block validation:
+    if modifiersConfig.kind != TomlValueKind.Array or
+       keyConfig.kind != TomlValueKind.String:
+      raise newException(Exception, "Invalid key configuration!")
+
+  let modifierArray = modifiersConfig.arrayVal
+  let modifiers: int = xorModifiers(modifierArray)
+  let keycode: int = keyConfig.stringVal.toKeycode(display)
+  return (keycode, modifiers)
+
+proc populateAction(display: PDisplay, action: string, loadedConfig: TomlTable) =
+  let keyCombo = loadedConfig.getKeyCombo(display, action)
+  ConfigTable[keyCombo] = action
+
+proc loadConfigfile(configPath: string): TomlTable =
+  ## Reads the user's configuration file into a table.
+  let loadedConfig = parsetoml.parseFile(configPath)
+  if loadedConfig.kind != TomlValueKind.Table:
+    raise newException(Exception, "Invalid key configuration!")
+  return loadedConfig.tableVal[]
+
+proc populateConfigTable*(display: PDisplay, configPath: string) =
+  ## Reads the user's configuration file and set the keybindings.
+  let configTable = loadConfigfile(configPath)
+  display.populateAction("testAction", configTable)
+  display.populateAction("testAction2", configTable)
 
--- src/nimdowpkg/keys/keyutils.nim
+++ src/nimdowpkg/keys/keyutils.nim
@@ -1,10 +1,27 @@
 import
-  x11 / x
+  x11 / [x, xlib],
+  tables
 
-func cleanMask*(mask: int): int = 
+## A table of key modifier names to their respective masks.
+const ModifierTable* = {
+  "control": ControlMask,
+  "shift": ShiftMask,
+  "alt": Mod1Mask,
+  "super": Mod4Mask,
+  "caps lock": LockMask,
+  "mod1": Mod1Mask,
+  "mod2": Mod2Mask,
+  "mod3": Mod3Mask,
+  "mod4": Mod4Mask,
+  "mod5": Mod5Mask,
+}.toTable
+
+func cleanMask*(mask: int): int =
   ## Creates a uniform mask that can be used with
   ## masks defined in x11/x.nim and TXKeyEvent.state
   mask and (not LockMask) and
   (ShiftMask or ControlMask or Mod1Mask or Mod2Mask or Mod3Mask or Mod4Mask or Mod5Mask)
 
+func toKeycode*(key: string, display: PDisplay): int =
+  return int(XKeysymToKeycode(display, XStringToKeysym("d")))
 
