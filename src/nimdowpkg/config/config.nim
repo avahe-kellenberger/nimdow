3382e216e6c126441679338bedbb63abf62a4c5a
Avahe Kellenberger <avahe@protonmail.ch>
2020-05-21 00:38:53 -0400

    Changed config to allow starting external commands

--- src/nimdowpkg/config/config.nim
+++ src/nimdowpkg/config/config.nim
@@ -1,5 +1,6 @@
 import 
   os,
+  osproc,
   parsetoml,
   tables,
   x11 / [x,  xlib],
@@ -8,41 +9,76 @@ import
 
 type
   KeyCombo* = tuple[keycode: int, modifiers: int]
-  WindowAction* = proc(keycode: int): void
+  Action* = proc(keycode: int): void
 
 # Mapping of config keys to functions.
-var ProcTable = initTable[string, WindowAction]()
-var ConfigTable* = initTable[KeyCombo, WindowAction]()
+var IdentifierTable = initTable[string, Action]()
+var KeyComboTable* = initTable[KeyCombo, Action]()
 
-proc configureAction*(actionName: string, actionInvokee: WindowAction)
+proc configureAction*(actionName: string, actionInvokee: Action)
 proc hookConfig*(eventManager: XEventManager)
-proc populateConfigTable*(display: PDisplay)
+proc populateKeyComboTable*(display: PDisplay)
 proc findConfigPath(): string
 proc loadConfigfile(configPath: string): TomlTable
-proc populateAction(display: PDisplay, action: string, configTable: TomlTable)
+proc populateControlAction(display: PDisplay, action: string, configTable: TomlTable)
 proc getKeyCombos(configTable: TomlTable, display: PDisplay, action: string): seq[KeyCombo]
 proc getKeysForAction(configTable: TomlTable, action: string): seq[string]
 proc getModifiersForAction(configTable: TomlTable, action: string): seq[TomlValueRef]
 proc xorModifiers(modifiers: openarray[TomlValueRef]): int
 proc getModifierMask(modifier: TomlValueRef): int
 
-proc configureAction*(actionName: string, actionInvokee: WindowAction) =
-  ProcTable[actionName] = actionInvokee
+proc configureAction*(actionName: string, actionInvokee: Action) =
+  IdentifierTable[actionName] = actionInvokee
+
+proc configureExternalProcess(command: string) =
+  IdentifierTable[command] =
+    proc(keycode: int) =
+      var process: Process
+      try:
+        process = startProcess(command = command, options = { poEvalCommand })
+      except:
+        echo "Failed to start command: ", command
+      finally:
+        if process != nil:
+          process.close
 
 proc hookConfig*(eventManager: XEventManager) =
   let listener: XEventListener = proc(e: TXEvent) =
     let mask: int = cleanMask(int(e.xkey.state))
     let keyCombo: KeyCombo = (int(e.xkey.keycode), mask)
-    if ConfigTable.hasKey(keyCombo):
-      ConfigTable[keyCombo](keyCombo.keycode)
+    if KeyComboTable.hasKey(keyCombo):
+      KeyComboTable[keyCombo](keyCombo.keycode)
   eventManager.addListener(listener, KeyPress)
 
-proc populateConfigTable*(display: PDisplay) =
+proc populateKeyComboTable*(display: PDisplay) =
   ## Reads the user's configuration file and set the keybindings.
   let configPath = findConfigPath()
   let configTable = loadConfigfile(configPath)
-  for action in configTable.keys():
-    display.populateAction(action, configTable)
+  # Populate window manager controls
+  let controlsTable = configTable["controls"]
+  if controlsTable.kind != TomlValueKind.Table:
+    raise newException(Exception, "Invalid config table!")
+  for action in controlsTable.tableVal[].keys():
+    display.populateControlAction(action, controlsTable[action].tableVal[])
+
+  # Populate external commands
+  let externalProcessesTable = configTable["startProcess"]
+  if externalProcessesTable.kind != TomlValueKind.Array:
+    echo "No \"startProcess\" commands defined!"
+  else:
+    for commandDeclaration in externalProcessesTable.arrayVal:
+      if commandDeclaration.kind != TomlValueKind.Table:
+        echo "Invalid \"startProcess\" configuration command!"
+        continue
+      if not commandDeclaration.tableVal[].hasKey("command"):
+        echo "Invalid \"startProcess\" configuration: Missing\"command\" string!"
+        continue
+      let command = commandDeclaration.tableVal["command"].stringVal
+      configureExternalProcess(command)
+      display.populateControlAction(
+        command,
+        commandDeclaration.tableVal[]
+      )
 
 proc findConfigPath(): string =
   let configHome = os.getConfigDir()
@@ -54,14 +90,14 @@ proc loadConfigfile(configPath: string): TomlTable =
   ## Reads the user's configuration file into a table.
   let loadedConfig = parsetoml.parseFile(configPath)
   if loadedConfig.kind != TomlValueKind.Table:
-    raise newException(Exception, "Invalid key configuration!")
+    raise newException(Exception, "Invalid config file!")
   return loadedConfig.tableVal[]
 
-proc populateAction(display: PDisplay, action: string, configTable: TomlTable) =
+proc populateControlAction(display: PDisplay, action: string, configTable: TomlTable) =
   let keyCombos = configTable.getKeyCombos(display, action)
   for keyCombo in keyCombos:
-    if ProcTable.hasKey(action):
-      ConfigTable[keyCombo] = ProcTable[action]
+    if IdentifierTable.hasKey(action):
+      KeyComboTable[keyCombo] = IdentifierTable[action]
     else:
       echo "Invalid key config action: \"", action, "\" does not exist"
 
@@ -75,11 +111,10 @@ proc getKeyCombos(configTable: TomlTable, display: PDisplay, action: string): se
     result.add((keycode, modifiers))
 
 proc getKeysForAction(configTable: TomlTable, action: string): seq[string] =
-  var tomlKeys = configTable[action]["keys"]
+  var tomlKeys = configTable["keys"]
   if tomlKeys.kind != TomlValueKind.Array:
     raise newException(Exception, "Invalid key config for action: " & action &
                        "\n\"keys\" must be an array of strings")
-
   for tomlKey in tomlKeys.arrayVal:
     if tomlKey.kind != TomlValueKind.String:
       raise newException(Exception, "Invalid key configuration: " &
@@ -87,7 +122,7 @@ proc getKeysForAction(configTable: TomlTable, action: string): seq[string] =
     result.add(tomlKey.stringVal)
 
 proc getModifiersForAction(configTable: TomlTable, action: string): seq[TomlValueRef] =
-  var modifiersConfig = configTable[action]["modifiers"]
+  var modifiersConfig = configTable["modifiers"]
   if modifiersConfig.kind != TomlValueKind.Array:
     raise newException(Exception, "Invalid key configuration: " &
                        repr(modifiersConfig) & " is not an array")
