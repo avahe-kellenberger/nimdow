f84ec102c32511a485b525516db16697657da783
Avahe Kellenberger <avahe@protonmail.ch>
2020-06-18 20:12:40 -0400

    Updated versions.

--- nimdow.nimble
+++ nimdow.nimble
@@ -1,5 +1,5 @@
 # Package
-version       = "0.5.3"
+version       = "0.5.4"
 author        = "avahe-kellenberger"
 description   = "A window manager written in nim"
 license       = "GPL v2"
--- src/nimdow.nim
+++ src/nimdow.nim
@@ -15,7 +15,7 @@ when isMainModule:
     if params.len == 1:
       let param = params[0].string
       if param == "-v" or param == "--version":
-        quit "Nimdow v0.5.3"
+        quit "Nimdow v0.5.4"
       else:
         # If given a parameter for a config file, use it instead of the default.
         configLoc = params[0].string
