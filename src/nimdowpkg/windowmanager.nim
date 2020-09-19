78343db0d7532d0754477c332e67931c523b8457
dakyskye <kanteladzelasha339@gmail.com>
2020-09-19 21:53:36 +0400

    allow inlining statuses for different monitors

--- src/nimdowpkg/windowmanager.nim
+++ src/nimdowpkg/windowmanager.nim
@@ -1198,19 +1198,20 @@ proc renderStatus(this: WindowManager) =
   ## Renders the status on all status bars.
   var name: cstring
   if XFetchName(this.display, this.rootWindow, name.addr) == 1:
-    var status = $name
-    if status.startsWith("NIMDOW_MONITOR_INDEX="):
-      status.removePrefix("NIMDOW_MONITOR_INDEX=")
-      var monitorIndexAsStr = status[0..skipWhile(status, Digits)-1]
-      let monitorIndex = parseInt(monitorIndexAsStr)
-      if monitorIndex < 0 or monitorIndex > this.monitors.len - 1:
-        log("NIMDOW_MONITOR_INDEX value is out of the range", lvlError)
-        return
-      status.removePrefix(monitorIndexAsStr)
-      this.monitors[monitorIndex].statusBar.setStatus(status)
-    else:
+    let statuses = ($name).split("NIMDOW_MONITOR_INDEX=")
+    if statuses.len == 1:
       for monitor in this.monitors:
-        monitor.statusBar.setStatus(status)
+        monitor.statusBar.setStatus(statuses[0])
+    else:
+      for status in statuses[1 .. statuses.len - 1]:
+        var monitorIndexAsStr = status[0..skipWhile(status, Digits)-1]
+        let monitorIndex = parseInt(monitorIndexAsStr)
+        if monitorIndex < 0 or monitorIndex > this.monitors.len - 1:
+          log("NIMDOW_MONITOR_INDEX value is out of the range", lvlError)
+          return
+        var st = status
+        st.removePrefix(monitorIndexAsStr)
+        this.monitors[monitorIndex].statusBar.setStatus(st)
 
 proc windowToMonitor(this: WindowManager, window: Window): Monitor =
   for monitor in this.monitors:
