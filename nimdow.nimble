4d815bd90733900af6f802b7732e66802a0a16b7
Avahe Kellenberger <avahe@protonmail.ch>
2020-10-11 13:52:16 -0400

    v0.7.4

--- nimdow.nimble
+++ nimdow.nimble
@@ -1,5 +1,5 @@
 # Package
-version       = "0.7.3"
+version       = "0.7.4"
 author        = "avahe-kellenberger"
 description   = "A window manager written in nim"
 license       = "GPL v2"
--- src/nimdow.nim
+++ src/nimdow.nim
@@ -6,7 +6,7 @@ import
   nimdowpkg/logger
 
 when isMainModule:
-  const version = "v0.7.3"
+  const version = "v0.7.4"
   when declared(commandLineParams):
     let params = commandLineParams()
     if params.len == 1:
