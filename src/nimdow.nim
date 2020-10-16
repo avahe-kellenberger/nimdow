37b3ff97dd9c6f9e9849e7b262ae2e31e3b43899
Avahe Kellenberger <avahe@protonmail.ch>
2020-10-16 16:17:21 -0400

    v0.7.8

--- src/nimdow.nim
+++ src/nimdow.nim
@@ -7,7 +7,7 @@ import
   nimdowpkg/logger
 
 when isMainModule:
-  const version = "v0.7.7"
+  const version = "v0.7.8"
   when declared(commandLineParams):
     let params = commandLineParams()
     if params.len == 1:
