63f14451fa7bb09045f7a0256970cc4b7cc7e22d
Avahe Kellenberger <avahe@protonmail.ch>
2022-08-29 09:28:07 -0400

    v0.7.33

--- nimdow.nimble
+++ nimdow.nimble
@@ -1,5 +1,5 @@
 # Package
-version = "0.7.32"
+version = "0.7.33"
 author = "avahe-kellenberger"
 description = "A window manager written in nim"
 license = "GPL v2"
--- src/nimdowpkg/ipc/cli.nim
+++ src/nimdowpkg/ipc/cli.nim
@@ -11,7 +11,7 @@ import
   ../wmcommands,
   ../logger
 
-const version* = "v0.7.32"
+const version* = "v0.7.33"
 const commit* = getEnv("LATEST_COMMIT")
 
 proc handleWMCommand(commandStr: string): bool =
