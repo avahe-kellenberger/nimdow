e83aafe953098670cdbc90ce9fafe97af1ad131e
DrunkenAlcoholic <drunkenalcoholic@gmail.com>
2024-02-03 13:11:28 +0800

    "Update to support weather intervals"

--- example status/src/functions/getWeather.nim
+++ example status/src/functions/getWeather.nim
@@ -1,13 +1,24 @@
-import std/httpclient
+import std/[httpclient, options]
 
 
 const
+  # set refresh frequency in minutes
+  INTERVALS: int = 10
   CITY: string = "Perth"
   ICON: string = "  "
-  
+
+var
+  dTimeStamp: Option[DateTime]
+  sWeather: string = ""
+
 proc getWeather(): string =
-  var hClient = newHttpClient()
-  try:
-    result = ICON & hClient.getContent("http://wttr.in/" & CITY & "?format=%t")
-  finally:
-    hClient.close()
+  if dTimeStamp.isSome and dTimeStamp.get() + initDuration(minutes = INTERVALS) > now():
+    result = sWeather # Do something with the weather information
+  else:
+    dTimeStamp = some(now())
+    var hClient = newHttpClient()
+    try:
+      sWeather = ICON & hClient.getContent("http://wttr.in/" & CITY & "?format=%t")
+      result = sWeather
+    finally:
+      hClient.close()
