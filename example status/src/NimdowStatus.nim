0bb9f10992ec435cd4aad621e807def96e0ada79
DrunkenAlcoholic <drunkenalcoholic@gmail.com>
2024-02-14 23:02:07 +0800

    Add files via upload

--- example status/src/NimdowStatus.nim
+++ example status/src/NimdowStatus.nim
@@ -1,4 +1,4 @@
-import std/[os, times, strutils, httpclient, options, strformat]
+import std/[os, osproc, times, strutils, httpclient, options, strformat]
 # import parsetoml  <--- WiP to move all the below settings to a toml file
 
 #++++++++++++++++++++++++++++++++++++++++++#
@@ -6,13 +6,15 @@ import std/[os, times, strutils, httpclient, options, strformat]
 #++++++++++++++++++++++++++++++++++++++++++#
 const
   ## main refresh intervals in seconds
-  UPDATE_INTERVAL = 40
+  UPDATE_INTERVAL = 10
   ## not used yet, more themes to come, see below theme section
   #THEME = "gruvbox_arrows"
   ## select city for weather, uses wttr.in
   CITY = "Perth" #
   ## update weather in minutes
   UPDATE_WEATHER = 10
+  ## update checkupdate intervals in minutes
+  UPDATE_UPDATES = 20
   ## typically its either BAT0 or BAT1, not used yet, see getBatStatus.nim to change
   #BAT = "BAT0"
   ## date formatting
@@ -29,13 +31,22 @@ const
   TIME_ICON = "  "
   ## memory icon to display
   MEMORY_ICON = "  "
+  ## Volume icon to display
+  VOL_ICON = " 󰕾 "
+  ## Mute icon to display
+  MUTE_ICON = " 󰖁 "
+  ## Keyboard Icon to display
+  KB_ICON = "  "
+  ## Update Icon to display
+  UPDATE_ICON = "   Updates: "
 
 #++++++++++++++++++++++++++++++++++++++++++#
 #                  THEME                   #
 #++++++++++++++++++++++++++++++++++++++++++#
 # include themed arrows gruvbox or dracula, more to come
 #include themes/gruvbox_arrows
-include themes/dracula_arrows
+#include themes/dracula_arrows
+include themes/nord_arrows
 
 
 #++++++++++++++++++++++++++++++++++++++++++#
@@ -49,18 +60,25 @@ include functions/getBatStatus
 include functions/getMemory
 # include weather function {getWeather()}
 include functions/getWeather
-
+# include Alsa volume levels function {getAlsa()}
+include functions/getAlsa
+# include keyboard layout function {getKeyboard()}
+include functions/getKeyboard
+# include updates from arch, function {getArchUpdates()}
+include functions/getArchUpdates
 
 
 # Function to set the string
-proc setStatus(sStatus: string) = 
+proc setStatus(sStatus: string) =
   discard execShellCmd("xsetroot -name " & "\"" & sStatus & "\"")
 
 # Main loop
 proc main() =
   while true:
-    # create the string using themed arrows and functions
-    let sStatusString = fmt"{ARROW_CYAN}{ARROW_ORANGE}{getWeather()}{ARROW_PINK}{getBatStatus()}{ARROW_PURPLE}{getMemory()}{ARROW_RED}{getDateTime()}{RESET}"
+    #+++++++++++++++++++++++++++
+    #  CREATE STATUS STRING    #
+    #+++++++++++++++++++++++++++
+    let sStatusString = fmt"{ARROW_6}{getArchUpdates()}{ARROW_7}{getWeather()}{ARROW_8}{getMemory()}{ARROW_9}{getBatStatus()}{ARROW_10}{getAlsa()}{ARROW_11}{getKeyboard()}{ARROW_12}{getDateTime()}{RESET}"
     #let sStatusString = fmt"{CIRCLE_GREEN_L}{getWeather()}{CIRCLE_GREEN_R}{CIRCLE_ORANGE_L}{getBatStatus()}{CIRCLE_ORANGE_R}{CIRCLE_BLUE_L}{getMemory()}{CIRCLE_BLUE_R}{CIRCLE_RED_L}{getDateTime()}{CIRCLE_RED_R}{RESET}"
     # set the status
     setStatus(sStatusString)
